54de72484627ceeaf49e1c41672a3d2ac2b634e5
Gary Coady <gary@lyranthe.org>
2025-04-12 14:14:49 +0200

    Add code that fails

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
+<script lang="ts">
+</script>
+
+{#snippet test(param: string = "default")}
+    <p>Inside a snippet, param: {param}</p>
+{/snippet}
+
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<p>
+    Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
+    documentation
+</p>
+{@render test()}
